5d7fab9e1dea55a085161fe85268b54b7f67c930
Hedi Bouassida <hbouassida.esct@gmail.com>
2025-04-08 11:52:58 -0400

    added method that calculates how much an ingridient was used in the recipies

--- test.py
+++ test.py
@@ -1,11 +1,14 @@
 import pandas as pd
-from salads_dataset import create_salad_df_from_initial_df, save_df_csv
+from salads_dataset import create_salad_df_from_initial_df, save_df_csv, ingrident_counter, get_salad_dataset
 from dataset_helper import change_df_ingredients_column_to_list
 pd.set_option('display.max_columns', None)     
 pd.set_option('display.max_colwidth', None)     
-pd.set_option('display.width', None)  
+pd.set_option('display.width', None)  \
 
-DATA_PATH = "C:/Users/yassi/Desktop/Projet/CookBotProject/CookBotRecipes/Data"
-
+DATA_PATH = "/home/hboua/GitRepo/Data"
+#DATA_PATH = "C:/Users/yassi/Desktop/Projet/CookBotProject/CookBotRecipes/Data"
+'''
 column_list = ['name', 'tags', 'description', 'ingredients']
 df = create_salad_df_from_initial_df(DATA_PATH,column_list)
+'''
+print(ingrident_counter(get_salad_dataset(DATA_PATH)))
