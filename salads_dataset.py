4a1cbd936c968c59c6b5c9f1b6d620bc3678fb94
Hedi Bouassida <hbouassida.esct@gmail.com>
2025-04-10 13:17:19 -0400

    fixed ingridient count method and added visualization method

--- salads_dataset.py
+++ salads_dataset.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 from dataset_helper import change_df_tags_column_to_list, change_df_ingredients_column_to_list, create_salad_dataset
 
 DATA_PATH = "/home/hboua/GitRepo/Data"
@@ -22,29 +24,51 @@ def save_df_csv(df, file_name, path_data_folder):
     file_path = path_data_folder + '/' + file_name + '.csv'
     df.to_csv(file_path, index=False)
 
-def ingrident_counter(df=get_salad_dataset(DATA_PATH)):
+def df_sorted_ingridient(df=get_salad_dataset(DATA_PATH)):
     change_df_ingredients_column_to_list(df)
-    ingridients_and_count = []
+    df_ingredient = {
+        "ingredient":[],
+        "count":[]
+    }
     
     # iterate through the rows of the dataframe
     for j in range(len(df)):
         list_ingredients = df.loc[j, 'ingredients']
         # iterate through the list of ingredients
         for i in range (len(list_ingredients)):
-            does_not_exist_in_list = True
-            ingridient = list_ingredients[i]
+            not_found = True
+            ingredient = list_ingredients[i]
             # check if the ingredient is already in the list
-            for ingridient_dic in ingridients_and_count:
-                if ingridient in ingridient_dic:
-                    does_not_exist_in_list = False
-                    ingridient_dic[ingridient] += 1
+            for item in list_ingredients:
+                if ingredient in df_ingredient["ingredient"]:
+                    not_found = False
+                    index = df_ingredient["ingredient"].index(ingredient)
+                    df_ingredient["count"][index] += 1
                     break
             # if not in list append
-            if does_not_exist_in_list:
-                ingridients_and_count.append({ingridient: 1})
-    ingridients_and_count = sorted(ingridients_and_count, key=lambda d: list(d.values())[0], reverse=True)
-    return ingridients_and_count 
-    
+            if not_found:
+                df_ingredient["ingredient"].append(ingredient)
+                df_ingredient["count"].append(1)
 
+    df_ingredient = pd.DataFrame(df_ingredient)   
+    df_sorted = df_ingredient.sort_values(by=['count'], ascending=False)   
+    return df_sorted
+
+def visulize_ingridient(df_sorted_ingridient=df_sorted_ingridient(get_salad_dataset(DATA_PATH))):
+   
+    df_sorted_ingridient = df_sorted_ingridient.head(80)
+    plt.figure(figsize=(20, 8))
+    plt.bar(df_sorted_ingridient['ingredient'], df_sorted_ingridient['count'])
+    plt.xlabel('Ingredient', fontsize=6)
+    plt.ylabel('Count')
+    plt.title('Ingredient Count (Sorted)')
+    plt.xticks(rotation=90, fontsize=6)
+    
+    plt.tight_layout()
+    plt.show()
+
+
+
+visulize_ingridient(df_sorted_ingridient(get_salad_dataset(DATA_PATH)))
 
 
