712d2c916947045448559930948c42aad1ccc47d
Hedi Bouassida <hbouassida.esct@gmail.com>
2025-04-13 15:07:06 -0400

    improving ingridient frequency graph

--- dataset_visualizer.py
+++ dataset_visualizer.py
@@ -1,7 +1,7 @@
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import pandas as pd
 
-def ingredient_frequency_graph(df):
+def ingredient_frequency_graph(df, first_n=80):
     """
     Visualizes the top 80 most common ingredients in a bar chart.
 
@@ -9,21 +9,42 @@ def ingredient_frequency_graph(df):
         df_sorted_ingridient (pandas.DataFrame): A DataFrame that must contain 
             at least two columns: 'ingredient' (str) and 'count' (int), 
             sorted in descending order by 'count'.
+        first_n (int): The number of top ingredients to display in the bar chart.
 
     Returns:
         None: Displays a bar chart of ingredient counts.
     """
     df_sorted_ingridient = sorted_ingridient_counter_df(df)
-    df_sorted_ingridient = df_sorted_ingridient.head(80)
-    plt.figure(figsize=(20, 8))
+    df_sorted_ingridient = df_sorted_ingridient.head(first_n)
+    plt.figure(figsize=(30, 10))
     plt.bar(df_sorted_ingridient['ingredients'], df_sorted_ingridient['count'])
     plt.xlabel('Ingredients', fontsize=6)
     plt.ylabel('Count')
     plt.title('Ingredient Count (Sorted)')
     plt.xticks(rotation=90, fontsize=6)
-    plt.tight_layout()
+    #plt.tight_layout()
     plt.show()
 
+
+def get_ingredient_count(df, ingredient):
+    '''
+    Retrieves the count of a specific ingredient from the sorted ingredient count DataFrame.
+
+    Args:
+        df (pd.DataFrame): A DataFrame with a column 'ingredients', where each row contains a list of strings.
+        ingredient (str): The ingredient whose count you want to retrieve.
+
+    Returns:
+        int: The count of the specified ingredient, or 0 if the ingredient is not found.
+    '''
+    sorted_df = sorted_ingridient_counter_df(df)
+    
+    ingredient_count = sorted_df.loc[sorted_df['ingredients'] == ingredient, 'count']
+    
+    if not ingredient_count.empty:
+        return ingredient_count.iloc[0]
+    else:
+        return 0
 
 # private method
 def sorted_ingridient_counter_df(df):
@@ -39,6 +60,7 @@ def sorted_ingridient_counter_df(df):
     Returns:
         pd.DataFrame: A DataFrame with columns 'ingredient' and 'count', sorted by count in descending order.
     '''
+    
     df_ingredient = {
         "ingredients":[],
         "count":[]
